e420edc80d6faa4b1a19b300930e433baea3167d
James Clagett <wclagett@engr.colostate.edu>
2025-07-15 10:53:59 -0600

    first commit

---
+++ source_data.R
@@ -0,0 +1,34 @@
+#-----------
+# Libraries
+library(tidyverse)
+library(plyr)
+library(faraway)
+library(cowplot)
+library(reshape2)
+library(corrplot)
+library(scales)
+library(ggthemes)
+library(gridExtra)
+library(patchwork)
+library(gghighlight)
+library(ggdark)
+library(viridis)
+library(DT)
+library(plotly)
+library(readxl)
+library(knitr)
+library(magick)
+library(corrr)
+library(googlesheets4)
+library(flextable)
+library(grid)
+
+#-----------
+#read omni data
+
+
+#read Sheets
+sheets_data <- read_sheet("https://docs.google.com/spreadsheets/d/1vqvEg71AyTTMJ6N4CC7I44UeyPgq4c12NMA-rjXQj7o/edit?gid=763198327#gid=763198327",
+                   "Calibration", col_types = "c")
+
+#read reference sensor data
